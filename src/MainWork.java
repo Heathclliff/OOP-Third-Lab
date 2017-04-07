d9d522ca665fe10422ee149af3a09e5296808fcc
Heathclliff <potapchik97@mail.ru>
2017-04-07 16:22:53 +0300

    some work with serializing and deserializing

---
+++ src/MainWork.java
@@ -0,0 +1,54 @@
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import organisms.AliveOrganism;
+
+import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ * Created by Валерий on 07.04.2017.
+ */
+public class MainWork {
+
+    public void serialize(AliveOrganism aliveOrganism){
+        String fileName;
+        fileName=getFileName();
+
+        XStream xStream = new XStream();
+        try {
+            FileOutputStream fs = new FileOutputStream(fileName);
+            xStream.toXML(aliveOrganism, fs);
+        } catch (FileNotFoundException e1) {
+            JOptionPane.showMessageDialog(null,
+                    "Incorrect File Name",
+                    "Please enter correct file name",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public String getFileName(){
+        String fileName = "";
+
+        return fileName;
+    }
+
+    public AliveOrganism deserialize(){
+        String fileName;
+        fileName=getFileName();
+
+        AliveOrganism aliveOrganism = null;
+        XStream xStream = new XStream(new DomDriver());
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            xStream.fromXML(fis, aliveOrganism);
+        } catch (FileNotFoundException e1) {
+            JOptionPane.showMessageDialog(null,
+                    "Incorrect File Name",
+                    "Please enter correct file name",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+        return aliveOrganism;
+    }
+}
